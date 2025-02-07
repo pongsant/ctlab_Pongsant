a13a6bf178ddb2720e9ad88981308d10183cbc5e
pongsant <1pongsant@gmail.com>
2025-02-07 11:20:12 -0500

    update

--- myproject-main-2/Assets/Myscript/playercontroller.cs
+++ myproject-main-2/Assets/Myscript/playercontroller.cs
@@ -1,26 +1,34 @@
 using UnityEngine;
 
 public class playercontroller : MonoBehaviour
-{
+
+{public float speed = 5.0f;
+private Rigidbody rb;
+public float rotationSpeed = 120f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        public float speed = 5.0f;
-        private
+
+    
     }
+
 
     // Update is called once per frame
     void Update()
     {
         rb = GetComponent<Rigidbody>();
-        void Start()
+
+
     }
 
-    private void FixedUpdate(){
-        //getting keyboard input andstrin in variable
-        float moveVertical = Input.GetAxxis("Vertical");
-        Vector3 movement = transform.forward * moveVertical * speed * Time.fixedDeltaTime;
-       // Debug.Log(movement);
-       rb.MovePosition(rb.MovePosition)
+        private void FixedUpdate(){
+        float moveVertical = Input.GetAxis("Vertical"); 
+        Vector3 movement = transform.forward * moveVertical * speed * Time.fixedDeltaTime; 
+        rb.MovePosition(rb.position + movement); 
+
+        float turn = Input.GetAxis("Horizontal") * rotationSpeed * Time.fixedDeltaTime; 
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f); 
+        rb.MoveRotation(rb.rotation * turnRotation); 
     }
+    
 }
